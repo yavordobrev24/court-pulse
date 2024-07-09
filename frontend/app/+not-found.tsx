d736e13006fa590a8f6b2eb952bf82b13bb20f53
Yavor Dobrev <yavordobrev24@gmail.com>
2024-07-09 16:37:00 +0300

    Create NotFound component for invalid pages

---
+++ frontend/app/+not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const NotFound = () => {
+  return (
+    <View>
+      <Text>Not found</Text>
+    </View>
+  )
+}
+
+export default NotFound
