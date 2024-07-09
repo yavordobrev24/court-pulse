5de12de29abdde403246757826954bb618a02eb2
Yavor Dobrev <yavordobrev24@gmail.com>
2024-07-09 16:52:07 +0300

    Create Team type

--- frontend/app/types.ts
+++ frontend/app/types.ts
@@ -20,3 +20,46 @@ export type Game = {
   NeutralVenue: boolean
   DateTimeUTC: string
 }
+
+export type Team = {
+  TeamID: number
+  Key: string
+  Active: boolean
+  City: string
+  Name: string
+  LeagueID: number
+  StadiumID: number
+  Conference: string
+  Division: string
+  PrimaryColor: string
+  SecondaryColor: string
+  TertiaryColor: string
+  QuaternaryColor: string
+  WikipediaLogoUrl: string
+  WikipediaWordMarkUrl: string | null
+  GlobalTeamID: number
+  NbaDotComTeamID: number
+  HeadCoach: string
+  Season: number
+  SeasonType: number
+  Wins: number
+  Losses: number
+  Percentage: number
+  ConferenceWins: number
+  ConferenceLosses: number
+  DivisionWins: number
+  DivisionLosses: number
+  HomeWins: number
+  HomeLosses: number
+  AwayWins: number
+  AwayLosses: number
+  LastTenWins: number
+  LastTenLosses: number
+  PointsPerGameFor: number
+  PointsPerGameAgainst: number
+  Streak: number
+  GamesBack: number
+  StreakDescription: string
+  ConferenceRank: number
+  DivisionRank: number
+}
