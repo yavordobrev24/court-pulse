7604676500e0e911284a6e90609ea8f50be5d659
Yavor Dobrev <yavordobrev24@gmail.com>
2024-07-09 16:25:39 +0300

    Enable CORS in the backend and define constants

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,17 @@
 import express, { Request, Response } from 'express'
 import { PrismaClient } from '@prisma/client'
 import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config()
 const PORT = process.env.PORT
 const app = express()
 const prisma = new PrismaClient()
+const NBA_STATS_API_KEY = process.env.NBA_STATS_API_KEY
+const NBA_STATS_API_URL = process.env.NBA_STATS_API_URL
+const NBA_NEWS_API_KEY = process.env.NBA_NEWS_API_KEY
+const NBA_NEWS_API_URL = process.env.NBA_NEWS_API_URL
 
+app.use(cors())
 app.use(express.json())
 app.get('/', (req: Request, res: Response) => {
   res.send('Index works')
